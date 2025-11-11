0b0025048dd699524c52504b9ed7398c7979d2e0
Victor-Muniu <victornjoroge2020@gmail.com>
2025-11-11 11:24:15 +0300

    Added Google Search Console

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.epashikinoresort.com',
+  generateRobotsTxt: true, // Generates robots.txt as well
+  sitemapSize: 7000,
+};
